6dd62bf8f9c78058099bcedd7421dc94735415e3
eaton <eatondoniana@gmail.com>
2025-06-21 00:48:19 -0400

    Added a models.py to books

--- books/models.py
+++ books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Book(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+    date_read = models.DateField()
+    
+    def __str__(self):
+        return self.title
