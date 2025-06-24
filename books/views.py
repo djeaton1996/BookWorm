54c5de1522a602671533912e555c448cd0f9253f
eaton <eatondoniana@gmail.com>
2025-06-23 23:23:25 -0400

    Added homepage view with register link and clean up URL routing

--- books/views.py
+++ books/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.urls import reverse
 
 def home(request):
-    return HttpResponse("<h1>Hello BookWorm!</h1>")
+    register_url = reverse('register')
+    return HttpResponse(f"""
+                        <h1>Hello Bookworm!</h1>
+                        <p>Welcome to your book tracking app.</p>
+                        <p><a href="{register_url}">Create an Account</a></p>
+                        """)
