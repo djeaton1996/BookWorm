80979774bf7e2af654a514dc4c0ef6835a7364fe
eaton <eatondoniana@gmail.com>
2025-06-24 00:30:42 -0400

    Added login and dashboard

--- books/views.py
+++ books/views.py
@@ -4,8 +4,10 @@ from django.urls import reverse
 
 def home(request):
     register_url = reverse('register')
+    login_url = reverse('login')
     return HttpResponse(f"""
                         <h1>Hello Bookworm!</h1>
                         <p>Welcome to your book tracking app.</p>
                         <p><a href="{register_url}">Create an Account</a></p>
+                        <p><a href="{login_url}">Log In</a></p>
                         """)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -8,9 +9,13 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('dashboard')
     else:
         form = UserCreationForm()
         
     return render(request, 'users/register.html', {'form': form})
 
+@login_required
+def dashboard(request):
+    user = request.user
+    return render(request, 'users/dashboard.html', {'user': user})
