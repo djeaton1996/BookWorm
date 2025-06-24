0fad78aa5a855485fcea58d3100931b2c9bf00b4
eaton <eatondoniana@gmail.com>
2025-06-23 22:58:21 -0400

    Add user registration, view, URL, and template

--- bookworm/urls.py
+++ bookworm/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('books.urls')),
+    path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
 ]
